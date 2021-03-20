72a96c42c61a983f7ce1dc8bc40fb4cf7f958d6a
bman150 <63990801+bman150@users.noreply.github.com>
2021-03-19 22:22:40 -0700

    Halloween

--- horror/views.py
+++ horror/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import HorrorSubGenre, Movie
 
 # Create your views here.
+# Return index for horror app.
 def index(request):
-    return render(request, 'horror/index.html')
+    return render(request, 'horror/index.html') 
+
+# Retrieves all the horror sub-genres.
+def getgenres(request):
+    genre_list=HorrorSubGenre.objects.all() 
+    return render(request, 'horror/genres.html', {'genre_list' : genre_list})
+
+# Retrieves all the horror movies
+def getmovies(request):
+    movie_list=Movie.objects.all()
+    return render(request, 'horror/movies.html', {'movie_list' : movie_list})
