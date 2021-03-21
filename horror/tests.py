a44ece2f8b4bca32233016df3c60887ad27d029a
bman150 <63990801+bman150@users.noreply.github.com>
2021-03-21 09:27:30 -0700

    Fright Night

--- horror/tests.py
+++ horror/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import HorrorSubGenre, Movie
 
 # Create your tests here.
+class HorrorSubGenreTest(TestCase):
+    def setUp(self):
+        self.genre=HorrorSubGenre(genrename='Haunted House')
+
+    def test_typestring(self):
+        self.assertEqual(str(self.genre), 'Haunted House')
